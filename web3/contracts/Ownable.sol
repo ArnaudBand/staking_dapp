c8357c0327bb8ee504d0cddb3b9c2c89916fed79
ArnaudBand <bandonkeyea@gmail.com>
2023-08-31 10:48:14 +0300

    create an event to transfer the ownership

---
+++ web3/contracts/Ownable.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// IMPORTING CONTRACT
+import "./Context.sol";
+
+// CONTRACT
+abstract contract Ownable is Context {
+  address private _owner;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  constructor() {
+    _transferOwnership(_msgSender());
+  }
+}
