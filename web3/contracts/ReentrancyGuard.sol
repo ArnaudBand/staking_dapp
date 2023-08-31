6157cc5e83a485e6aea03f8b407739c6d11f1fb8
ArnaudBand <bandonkeyea@gmail.com>
2023-08-31 11:47:30 +0300

    Create Reentrancyguard for protecting our smart contract

---
+++ web3/contracts/ReentrancyGuard.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.9;
+
+abstract contract ReentrancyGuard {
+  uint256 private constant _NOT_ENTERED = 1;
+  uint256 private constant _ENTERED = 2;
+
+  uint256 private _status;
+
+  constructor() {
+    _status = _NOT_ENTERED;
+  }
+
+  // MODIFIER FUNCTION TO CHECK IF CONTRACT IS NOT ENTERED
+  modifier nonReentrant() {
+    require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+    _status = _ENTERED;
+    _;
+    _status = _NOT_ENTERED;
+  }
+}
