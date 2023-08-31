774125fb56e69c745069e1d96d9509bf9234434a
ArnaudBand <bandonkeyea@gmail.com>
2023-08-31 11:21:42 +0300

    Finalize the ownable smart contract

--- web3/contracts/Ownable.sol
+++ web3/contracts/Ownable.sol
@@ -35,4 +35,10 @@ abstract contract Ownable is Context {
     require(newOwner != address(0), "Ownable: new owner is the zero address");
     _transferOwnership(newOwner);
   }
+
+  function _transferOwnership(address newOwner) internal virtual {
+    address oldOwner = _owner;
+    _owner = newOwner;
+    emit OwnershipTransferred(oldOwner, newOwner);
+  }
 }
