e57df47da620f4abae9394fe69a4f2c2a2149b12
ArnaudBand <bandonkeyea@gmail.com>
2023-09-02 02:10:47 +0300

    Check the connection, and handle web3 events

--- js/common.js
+++ js/common.js
@@ -27,3 +27,62 @@ async function commonProviderDetector(_provider) {
     }
   }
 };
+
+async function commonInjectedConnect(_provider, _provider_name) {
+  await _provider.enable();
+  setWeb3Events(_provider);
+
+  web3 = new Web3(_provider);
+
+  // Get the current chain id
+  let currentNetworkId = await web3.eth.getChainId();
+  currentNetworkId = currentNetworkId.toString();
+  console.log("currentNetworkId", currentNetworkId);
+
+  // Get the current account
+  let accounts = await web3.eth.getAccounts();
+  console.log("accounts", accounts);
+
+  currentAddress = accounts[0].toLowerCase();
+
+  // Check if the current network is supported
+  if(currentNetworkId != _NETWORK_ID) {
+    console.log("Network not supported");
+    notyf.error(`Please connect Wallet on ${SELECT_CONTRACT[_NETWORK_ID].network_name}`);
+    return false;
+  }
+  oContractToken = new web3.eth.Contract(SELECT_CONTRACT[_NETWORK_ID].TOKEN.abi, SELECT_CONTRACT[_NETWORK_ID].TOKEN.address);
+  return true;
+};
+
+function setWeb3Events(_provider) {
+  _provider.on("accountsChanged", function (accounts) {
+    console.log("accountsChanged", accounts);
+    if(!accounts.length) {
+      logout();
+    } else {
+      currentAddress = accounts[0];
+      let sClass = getSelectedTab();
+    }
+  });
+
+  // Handle chainChanged
+  _provider.on("chainChanged", function (chainId) {
+    console.log("chainChanged", chainId);
+    logout();
+  });
+
+  // Handle session connection
+  _provider.on("connect", function () {
+    console.log("connect");
+    logout();
+  });
+
+  // Handle session disconnection
+  _provider.on("disconnect", function (code, reason) {
+    console.log("disconnect");
+    localStorage.clear();
+    logout();
+  });
+
+}
