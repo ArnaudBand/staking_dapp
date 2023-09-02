5a0dfe6a0e76e9d9b051e16990419b5b5e3b928e
ArnaudBand <bandonkeyea@gmail.com>
2023-09-02 10:30:43 +0300

    Fix the network error

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -3,15 +3,15 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const RPC_URL = "https://rpc.ankr.com/polygon_mumbai";
+const RPC_URL = "https://rpc.ankr.com/mumbai";
 module.exports = {
-  defaultNetwork: "polygon_mumbai",
+  defaultNetwork: "mumbai",
   networks: {
     hardhat: {
       chainId: 80001,
     },
     mumbai: {
-      url: "https://rpc.ankr.com/polygon_mumbai",
+      url: "https://rpc.ankr.com/mumbai",
       accounts: [`0x${PRIVATE_KEY}`],
     },
   },
