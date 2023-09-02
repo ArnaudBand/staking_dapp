e5222240b1872d62162570baed7837f6ebeaa8be
ArnaudBand <bandonkeyea@gmail.com>
2023-09-02 10:37:32 +0300

    Get the contract object

--- js/index.js
+++ js/index.js
@@ -9,3 +9,19 @@ function openTab(event, name) {
   getSelectedTab(name);
   loadInitialData(name);
 }
+
+
+const loadInitialData = async (sClass) => {
+  console.log("loadInitialData", sClass);
+
+  try {
+    clearInterval(countdownGlobal);
+
+    let cObj = new web3Main.eth.Contract(
+      SELECT_CONTRACT[_NETWORK_ID].STAKING.abi,
+      SELECT_CONTRACT[_NETWORK_ID].STAKING[sClass].address
+    );
+  } catch (error) {
+    
+  }
+}
