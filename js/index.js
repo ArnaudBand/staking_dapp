16725830bd4ff152ba8fba20f594e9b468430f7c
ArnaudBand <bandonkeyea@gmail.com>
2023-09-11 22:52:33 +0300

    Implement the countdown

--- js/index.js
+++ js/index.js
@@ -142,3 +142,25 @@ const loadInitialData = async (sClass) => {
     )
   }
 }
+
+function generateCountdown(ele, claimDate) {
+  clearInterval(countdownGlobal);
+  var countdownDate = new Date(claimDate).getTime();
+
+  countdownGlobal = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countdownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) /  (1000 * 60)); 
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    ele.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+    if (distance < 0) {
+      clearInterval(countdownGlobal);
+      ele.innerHTML = "EXPIRED";
+    }
+  }, 1000);
+}
